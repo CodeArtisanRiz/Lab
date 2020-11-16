b1c76161a06245e9681e902b551794c55cf8bca1
Sourav Deb <54738492+souravdeb299@users.noreply.github.com>
2020-11-16 19:37:19 +0530

    Update docList.php

--- assets/process/docList.php
+++ assets/process/docList.php
@@ -1,17 +1,18 @@
 <?php
 
-include 'assets\conn\conn.php';
+    include 'assets\conn\conn.php';
 
-$selectquery = " select * from doctor ";
+    $selectquery = " select * from doctor ";
 
-$query = mysqli_query($con,$selectquery);
+    $query = mysqli_query($con,$selectquery);
 
-$nums = mysqli_num_rows($query);
+    $nums = mysqli_num_rows($query);
 
-while($res = mysqli_fetch_array($query)){
-    echo $res['dname'] . "<br>";
-}
+    while($res = mysqli_fetch_array($query)){
+        echo $res['dname'] . "<br>";
+    }
+    
+    //This page is to under how listing module works.
+    //This page is not functional.
+
 ?>
-
-//This page is to under how listing module works.
-//This page is not functional.
