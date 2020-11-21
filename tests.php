86eecd3f3c2cf5b99d1e9e8572dfc433b01c40dc
xtremist96 <riz1.xtreme@gmail.com>
2020-11-22 01:34:34 +0530

    search added

--- tests.php
+++ tests.php
@@ -11,6 +11,8 @@
     <title>Lab - Tests</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    
+    <link rel="stylesheet" href="samples/select2.min.css" />
 </head>
 
 <body>
@@ -131,6 +133,14 @@
     <script src="check_phone.js"></script>
     <script src="https://use.fontawesome.com/0a050084c3.js"></script>
     <!-- <script src="dropdown.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="samples/select2.min.js"></script>
+    <script>
+        $("#id_vial").select2({
+            placeholder: "Select Vial",
+            allowClear: true
+        });
+    </script>
 </body>
 
 </html>
