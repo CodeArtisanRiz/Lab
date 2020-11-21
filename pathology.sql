703a104dbf09aa145e403bd65dd043f7b65788d2
Sourav Deb <54738492+souravdeb299@users.noreply.github.com>
2020-11-21 22:36:45 +0530

    Update pathology.sql
    Database SQL file

--- pathology.sql
+++ pathology.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2020 at 05:06 PM
+-- Generation Time: Nov 21, 2020 at 06:05 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.6
 
@@ -24,27 +24,6 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data`
---
-
-CREATE TABLE `data` (
-  `dname` varchar(255) NOT NULL,
-  `quali` varchar(255) NOT NULL,
-  `special` varchar(255) NOT NULL,
-  `mobile` bigint(20) NOT NULL,
-  `refcent` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `data`
---
-
-INSERT INTO `data` (`dname`, `quali`, `special`, `mobile`, `refcent`) VALUES
-('Sourav Deb', 'B.Sc Computer Science', 'Web Development', 7002246506, 2000);
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `doctor`
 --
 
@@ -54,7 +33,7 @@ CREATE TABLE `doctor` (
   `mobile` bigint(10) NOT NULL,
   `quali` varchar(255) NOT NULL,
   `special` varchar(255) NOT NULL,
-  `refcent` decimal(10,0) NOT NULL
+  `refcent` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -62,8 +41,23 @@ CREATE TABLE `doctor` (
 --
 
 INSERT INTO `doctor` (`did`, `dname`, `mobile`, `quali`, `special`, `refcent`) VALUES
-(21, 'hello', 45, 'hello', 'hello', '45'),
-(22, 'hello', 45, 'hello', 'hello', '45');
+(34, 'RamRam', 1234567890, 'MBBS', 'ENT', '25.87'),
+(35, 'hello', 4569871230, 'B.Sc Computer Science', 'Web Development', '25.26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tests`
+--
+
+CREATE TABLE `tests` (
+  `tid` bigint(10) NOT NULL,
+  `tcode` varchar(255) NOT NULL,
+  `tname` varchar(255) NOT NULL,
+  `vial_type` varchar(255) NOT NULL,
+  `c_price` decimal(10,2) NOT NULL,
+  `s_price` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
@@ -76,6 +70,12 @@ ALTER TABLE `doctor`
   ADD PRIMARY KEY (`did`);
 
 --
+-- Indexes for table `tests`
+--
+ALTER TABLE `tests`
+  ADD PRIMARY KEY (`tid`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -83,7 +83,13 @@ ALTER TABLE `doctor`
 -- AUTO_INCREMENT for table `doctor`
 --
 ALTER TABLE `doctor`
-  MODIFY `did` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `did` bigint(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+
+--
+-- AUTO_INCREMENT for table `tests`
+--
+ALTER TABLE `tests`
+  MODIFY `tid` bigint(10) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
