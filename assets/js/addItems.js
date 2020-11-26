e6b910fec7282a7ce4e868610177ca9edd45446f
xtremist96 <riz1.xtreme@gmail.com>
2020-11-26 11:49:50 +0530

    Update addItems.js

--- assets/js/addItems.js
+++ assets/js/addItems.js
@@ -3,12 +3,12 @@ var a = 0;
 function change() {
     a = a + 1;
     // Selecting last id 
-    var lastname_id = $('.input-form input[type=text]:nth-child(1)').last().attr('id');
-    var split_id = lastname_id.split('_');
+    // var lastname_id = $('.input-form input[type=text]:nth-child(1)').last().attr('id');
+    // var split_id = lastname_id.split('_');
 
     // New index
-    var index = Number(split_id[1]) + 1;
-    // var index = a;
+    // var index = Number(split_id[1]) + 1;
+    var index = a;
 
     // Fix not responding select after clone
     if ($(".test_name").data('select2')) {
@@ -19,12 +19,12 @@ function change() {
     var newel = $('.input-form:last').clone(true);
 
     // Set id of new element
-    $(newel).find('input[name=test_code]').attr("id", "tCode_" + index);
-    // $(newel).find('input[type=number]:nth-child(2)').attr("id", "email_" + index);
-    $(newel).find('input[name=price]').attr("id", "tPrice_" + index);
+    $(newel).find('input[type=text]:nth-child(1)').attr("id", "tCode" + index);
+    // $(newel).find('input[type=text]:nth-child(1)').attr("id", "email_" + index);
+    $(newel).find('input[name=price]').attr("id", "tPrice" + index);
 
     // Set value
-    $(newel).find('.input[name=test_code]').val("tCode_" + index);
+    $(newel).find('input[type=text]:nth-child(1)').val("tCode_" + index);
     // $(newel).find('input[type=number]:nth-child(2)').val("email_" + index);
     $(newel).find('input[name=price]').val("tPrice_" + index);
 
