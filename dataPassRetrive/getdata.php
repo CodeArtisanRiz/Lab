56c7284a9a8cd4379368a61090d41fcfff5fdf43
Sourav Deb <54738492+souravdeb299@users.noreply.github.com>
2020-12-01 20:19:15 +0530

    Update getdata.php

--- dataPassRetrive/getdata.php
+++ dataPassRetrive/getdata.php
@@ -1,11 +1,35 @@
 <?php
 
+include 'conn.php';
+
+
 $val = $_GET['selectvalue'];
+
+//$val = 'Hello7';
+
+$selectquery = " SELECT * from tests where tname= '$val' ";
+
+$query = mysqli_query($con,$selectquery);
+
+$nums = mysqli_num_rows($query);
+
+    while($res = mysqli_fetch_array($query)){
+        //echo $res['tcode'];
+        $returnCode = $res['tcode'];
+        $returnPrice = $res['s_price'];
+    }
+
+    
+echo $returnCode;
+
+echo $returnPrice;
+   
 
 // $frame1 = array('1','2','3');
 
 
 // echo "<option> $val </option>";
-echo $val;
+//echo $val;
+
 
 ?>
